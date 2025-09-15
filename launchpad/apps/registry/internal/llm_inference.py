c1f718762ae8f3b437d23595ba508f8157662704
Jordy Antunes <jordy.antunes@hotmail.com>
2025-09-15 12:16:23 -0300

    Fixed vllm app name

--- launchpad/apps/registry/internal/llm_inference.py
+++ launchpad/apps/registry/internal/llm_inference.py
@@ -8,7 +8,7 @@ APP_NAME_LLM_INFERENCE = "vllm-llama-3.1-8b"
 
 class LlmInferenceApp(App[InternalAppContext]):
     name = APP_NAME_LLM_INFERENCE
-    template_name = "llm-inference"
+    template_name = "llm-inference-argocd"
     template_version = "v25.9.0"
     is_internal = True
     is_shared = True
