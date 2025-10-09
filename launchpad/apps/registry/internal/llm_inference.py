4afbb9e58f9413ff8218197ce99226c83eb4f913
Jordy Jackson Antunes da Rocha <jordy.antunes@hotmail.com>
2025-10-09 12:12:28 -0300

    Fix llm to workflow v25.10.0 (#128)

--- launchpad/apps/registry/internal/llm_inference.py
+++ launchpad/apps/registry/internal/llm_inference.py
@@ -8,8 +8,8 @@ APP_NAME_LLM_INFERENCE = "vllm-llama-3.1-8b"
 
 class LlmInferenceApp(App[InternalAppContext]):
     name = APP_NAME_LLM_INFERENCE
-    template_name = "llm-inference-argocd"
-    template_version = "latest"  # we can lock this later when app versioning is working
+    template_name = "llm-inference"
+    template_version = "v25.10.0"  # we can lock this later when app versioning is working
     is_internal = True
     is_shared = True
     verbose_name = "LLM Inference"
