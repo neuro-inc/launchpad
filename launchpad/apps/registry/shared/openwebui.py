343c29dd81b1dec8f16b28acfed3c2f41fb1d82a
Jordy Antunes <jordy.antunes@hotmail.com>
2025-10-09 18:14:26 -0300

    Trying to enable sign up with env var

--- launchpad/apps/registry/shared/openwebui.py
+++ launchpad/apps/registry/shared/openwebui.py
@@ -176,6 +176,10 @@ class OpenWebUIApp(App[OpenWebUIAppContext]):
                     {
                         "name": "GLOBAL_LOG_LEVEL",
                         "value": "DEBUG"
+                    },
+                    {
+                        "name": "ENABLE_LOGIN_FORM",
+                        "value": "false",
                     }
                 ]
             },
