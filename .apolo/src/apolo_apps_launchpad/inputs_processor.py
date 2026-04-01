64b89046aaab7de303ffc9644730a9c1f497dfde
Yevhenii Semendiak <semendyak@gmail.com>
2026-04-01 15:04:10 +0300

    upd

--- .apolo/src/apolo_apps_launchpad/inputs_processor.py
+++ .apolo/src/apolo_apps_launchpad/inputs_processor.py
@@ -488,9 +488,7 @@ class LaunchpadInputsProcessor(BaseChartValueProcessor[LaunchpadAppInputs]):
         if kc_extra_env_vars:
             keycloak_values["extraEnvVars"] = kc_extra_env_vars
 
-        cluster_config = await self.client._clusters.get_cluster(
-            self.client.config.cluster_name
-        )
+        cluster_config = self.client.config.clusters[self.client.cluster_name]
         if cluster_config.apps.launchpad_use_subdomain:
             values["clientSubdomain"] = True
 
